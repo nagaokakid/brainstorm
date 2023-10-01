315d94d29056ceda28577ee536064069cc49bb7f
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2023-10-01 10:19:04 -0500

    changed the app page to login page for testing

--- UI/src/main.jsx
+++ UI/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+//import App from './App.jsx'
+import LoginPage from './pages/LoginPage.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* Change the bottom line to test any pages */ }
+      <LoginPage />
   </React.StrictMode>,
 )
