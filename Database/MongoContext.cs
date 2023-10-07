41fd4bc25fdc9fb952ab0be043e4ec8f722bfe83
nagaokakid <nagaokakid@gmail.com>
2023-10-07 15:12:23 -0500

    Created generic class for Mongo DB collection. Completed CRUD operations for generic class using MongoDB driver.

---
+++ Database/MongoContext.cs
@@ -0,0 +1,64 @@
+﻿using MongoDB.Driver;
+using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using System.Linq.Expressions;
+
+namespace Database
+{
+    public class MongoContext
+    {
+        private readonly IMongoDatabase database;
+
+        public MongoContext()
+        {
+            try
+            {
+                // Get the current working directory
+                string currentDirectory = Directory.GetCurrentDirectory();
+
+                // Build the full path to the JSON configuration file
+                string configFilePath = Path.Combine(currentDirectory, "mongoSettings.json");
+
+                // Create a ConfigurationBuilder
+                var builder = new ConfigurationBuilder().AddJsonFile(configFilePath, optional: true, reloadOnChange: true);
+
+                // Build the IConfiguration
+                IConfigurationRoot configuration = builder.Build();
+
+                // Get all parts of the MongoDB connection string from the JSON config file
+                string username = configuration.GetConnectionString("ConnectionString:Username");
+                string apiKey = configuration.GetConnectionString("ConnectionString:API_Key");
+                string cluster = configuration.GetConnectionString("ConnectionString:Cluster");
+                string databaseName = configuration.GetConnectionString("ConnectionString:Database");
+
+                // Assemble the connection string
+                string connectionString = $"mongo+srv://{username}:{apiKey}@{cluster}.mongodb.net/{databaseName}";
+
+                // Establish a connection to Mongo Atlas and retrieve brainstorm app database
+                var client = new MongoClient(connectionString);
+                database = client.GetDatabase(databaseName);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Mongo settings JSON file not found.");
+            }
+            catch (MongoClientException ex)
+            {
+                Console.WriteLine($"Error with Mongo client: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading Mongo settings JSON file: {ex.Message}");
+            }
+
+        }
+
+        // Return a MongoDB collection given the collection name
+        public IMongoCollection<T> GetCollection<T>(string collectionName)
+        {
+            return database.GetCollection<T>(collectionName);
+        }
+
+    }
+}
---
+++ Database/MongoRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Database;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+// Generic class to represent a collection of T type objects (User, ChatRoom, Message, etc.)
+public class MongoRepository<T> where T : class
+{
+    private readonly IMongoCollection<T> collection;
+
+    public MongoRepository(MongoContext context, string collectionName)
+    {
+        collection = context.GetCollection<T>(collectionName);
+    }
+
+    public async Task<IEnumerable<T>> GetAll()
+    {
+        return await collection.Find(_ => true).ToListAsync();
+    }
+
+    public async Task<T> GetById(string id)
+    {
+        var objectId = new ObjectId(id);
+        return await collection.Find(item => item.Id.Equals(objectId)).FirstOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<T>> Find(Expression<Func<T, bool>> filter)
+    {
+        return await collection.Find(filter).ToListAsync();
+    }
+
+    public async Task Create(T entity)
+    {
+        await collection.InsertOneAsync(entity);
+    }
+
+    public async Task Update(string id, T entity)
+    {
+        var objectId = new ObjectId(id);
+        await collection.ReplaceOneAsync(item => item.Id.Equals(objectId), entity);
+    }
+
+    public async Task Delete(string id)
+    {
+        var objectId = new ObjectId(id);
+        await collection.DeleteOneAsync(item => item.Id.Equals(objectId));
+    }
+}
+
