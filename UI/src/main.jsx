71cee58da8a329eb8b8586c9391eb91f01d95120
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2023-10-09 19:32:23 -0500

    added chat room component for testing

--- UI/src/main.jsx
+++ UI/src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'
-import LoginPage from './pages/LoginPage.jsx'
+//import LoginPage from './pages/LoginPage.jsx'
+import ChatRoomOption from './components/chatRoomOption.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* Change the bottom line to test any pages */ }
-      <LoginPage />
+      <ChatRoomOption/>
   </React.StrictMode>,
 )
