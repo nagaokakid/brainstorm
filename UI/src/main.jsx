862d822bc0e9be30c4f872634d769ca689f01bf8
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2023-10-12 18:17:50 -0500

    Added msg input field for testing

--- UI/src/main.jsx
+++ UI/src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'
 //import LoginPage from './pages/LoginPage.jsx'
-import ChatRoomOption from './components/chatRoomOption.jsx'
+//import ChatRoomOption from './components/chatRoomOption.jsx'
+import MsgInput from './components/msgInputField.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* Change the bottom line to test any pages */ }
-      <ChatRoomOption/>
+      <MsgInput/>
   </React.StrictMode>,
 )
