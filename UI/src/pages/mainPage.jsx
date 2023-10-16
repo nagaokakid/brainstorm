707d8a8ff0c615a6ba673a1cbb3776c6a7f601e3
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2023-10-16 14:58:15 -0500

    Added get chat list function

--- UI/src/pages/mainPage.jsx
+++ UI/src/pages/mainPage.jsx
@@ -3,29 +3,48 @@ import HeaderNavBar from "../components/HeaderNavBar";
 import NavigationBar from "../components/NavigationBar";
 import ChatList from "../components/ChatList";
 import ChatRoomWindow from "../components/chatRoomWindow";
-import { useEffect, useState } from "react";
+import AppInfo  from "../services/appInfo";
+import { useEffect, useRef, useState } from "react";
 //top element a grid, 4 colms
 function MainPage()
 {
-
-  const [chatType, setChatType] = useState(1);
+  const [chatType, setChatType] = useState("Direct Message List");
+  const [chatList, setChatList] = useState([{}])
+  const [listId, setListId] = useState("Title")
+  const headerTitle = useRef("Title");
+  
+  // Handle the callback from the NavigationBar component
   const handleCallBack = (childData) => {
     setChatType(childData);
   }
-  const [chatList, setChatList] = useState(1)
+
+  // Update the chat list when the chat type changes
+  useEffect(() => {
+    if (chatType === "Direct Message List") {
+      setChatList(AppInfo.getDirectMessagesList());
+    } else if (chatType === "ChatRoom List") {
+      setChatList(AppInfo.getChatRoomsList());
+    }
+  }, [chatType]);
+
+  // Handle the callback from the ChatList component
   const handleCallBack2 = (childData) => {
-    setChatList(childData);
+    setListId(childData);
+    headerTitle.current = childData;
   }
+  useEffect(() => {
+    headerTitle.current = listId;
+  }, [listId, chatType]);
   
   return (
     <div className="App">
       <div className="headerNavContainer">
-        <HeaderNavBar userName={["userName"]} />
+        <HeaderNavBar />
       </div>
       <div className="main-page-container">
         <NavigationBar handleCallBack={handleCallBack}/>
-        <ChatList chats={3} handleCallBack={handleCallBack2}/>
-        <ChatRoomWindow window={chatList} chatType={chatType}/>
+        <ChatList list={chatList} chatType={chatType} handleCallBack={handleCallBack2}/>
+        <ChatRoomWindow headerTitle={headerTitle.current} chatType={chatType}/>
       </div>
     </div>
   );
