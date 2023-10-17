ed1253b613fd28802951466ad68cd3cfb68976f1
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2023-10-16 22:49:08 -0500

    Added receive msg function

--- UI/src/pages/mainPage.jsx
+++ UI/src/pages/mainPage.jsx
@@ -3,9 +3,27 @@ import HeaderNavBar from "../components/HeaderNavBar";
 import NavigationBar from "../components/NavigationBar";
 import ChatList from "../components/ChatList";
 import { useState } from "react";
+import SignalRChatRoom from "../services/chatRoomConnection";
+import SignalRDirect from "../services/directMessageConnection";
+import AppInfo from "../services/appInfo";
+
 //top element a grid, 4 colms
 function MainPage()
 {
+  const [isUpdated, setIsUpdated] = useState(false)
+
+  // Receive message from SignalR
+  function receiveMessage(message)
+  {
+    AppInfo.addMessage(message)
+    setIsUpdated(!isUpdated)
+  }
+
+  // Create SignalR connection
+  SignalRChatRoom.getInstance(receiveMessage)
+  SignalRDirect.getInstance(receiveMessage)
+
+  // Set the default chat type to be "Direct Message List"
   const [chatType, setChatType] = useState("Direct Message List");
   
   // Handle the callback from the NavigationBar component
@@ -21,7 +39,7 @@ function MainPage()
       </div>
       <div className="main-page-container">
         <NavigationBar handleCallBack={handleCallBack}/>
-        <ChatList chatType={chatType}/>
+        <ChatList chatType={chatType} changes= {isUpdated} />
       </div>
     </div>
   );
