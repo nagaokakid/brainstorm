9bd47212c416e497b7f2715b89cf79976a7a72cf
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2023-10-17 18:38:31 -0500

    modified the styling

--- UI/src/components/chatRoomOption.jsx
+++ UI/src/components/chatRoomOption.jsx
@@ -4,16 +4,16 @@ import icon1 from '../assets/chat-bubble.png'
 import icon2 from '../assets/meeting.png'
 import GuestJoin from './guestJoin'
 
-function chatRoomOption() {
-    const buttunHandler = (selected) => { }
+function chatRoomOption(props) {
+    const buttonHandler = (selected) => { }
     return (
-        <div className='PageContainer'>
+        <div className='OptionContainer' style={ {display : props.style}}>
             <div className="btn-group" role="group" aria-label="Basic example">
-                <button type="button" className="btn btn-primary" onClick={() => buttunHandler(1)}>
+                <button type="button" className="btn btn-primary" onClick={() => buttonHandler(1)}>
                     <img className='btn-icon' src={icon1} alt="" />
                     Create Chat Room
                 </button>
-                <button type="button" className="btn btn-primary" onClick={() => buttunHandler(2)}>
+                <button type="button" className="btn btn-primary" onClick={() => buttonHandler(2)}>
                     <img className='btn-icon' src={icon2} alt="" />
                     <GuestJoin />
                 </button>
