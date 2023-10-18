ec8abe913238d18202dfca5eec8be99edd37cac6
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2023-10-17 19:36:18 -0500

    Added create chat room function

--- UI/src/components/chatRoomOption.jsx
+++ UI/src/components/chatRoomOption.jsx
@@ -1,11 +1,24 @@
 import '../styles/chatRoomOption.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import icon1 from '../assets/chat-bubble.png'
 import icon2 from '../assets/meeting.png'
 import GuestJoin from './guestJoin'
+import AppInfo from '../services/appInfo'
+import ApiService from '../services/apiService'
 
 function chatRoomOption(props) {
-    const buttonHandler = (selected) => { }
+    const buttonHandler = (selected) =>
+    {
+        if (selected === 1)
+        {
+            var chatRoomName, description = prompt("Please enter the Chat room name", "Description");
+            if (chatRoomName != null && description != null)
+            {
+                var userId = AppInfo.getUserId();
+                const apiService = new ApiService();
+                apiService.createChatRoom(userId, chatRoomName, description);
+            }
+        }
+    }
     return (
         <div className='OptionContainer' style={ {display : props.style}}>
             <div className="btn-group" role="group" aria-label="Basic example">
@@ -13,7 +26,7 @@ function chatRoomOption(props) {
                     <img className='btn-icon' src={icon1} alt="" />
                     Create Chat Room
                 </button>
-                <button type="button" className="btn btn-primary" onClick={() => buttonHandler(2)}>
+                <button type="button" className="btn btn-primary" >
                     <img className='btn-icon' src={icon2} alt="" />
                     <GuestJoin />
                 </button>
