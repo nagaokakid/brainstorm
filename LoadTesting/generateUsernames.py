816aec821635e0ee9521f60774649e469c25ac9c
nagaokakid <nagaokakid@gmail.com>
2023-11-24 15:28:36 -0600

    Shortened usernames list.

--- LoadTesting/generateUsernames.py
+++ LoadTesting/generateUsernames.py
@@ -4,6 +4,6 @@ with open("usernames.csv", mode="w", newline='') as file:
     writer = csv.writer(file)
     baseUsername = 'loadTestUser'
 
-    for i in range(1, 101):
+    for i in range(1, 21):
         username = baseUsername + str(i)
         writer.writerow([username])    
