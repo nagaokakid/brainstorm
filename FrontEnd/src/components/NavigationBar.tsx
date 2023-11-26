0676dedb151b343b8df63ee4507b31d54b8bf600
singhr27 <144287500+singhr27@users.noreply.github.com>
2023-11-26 03:05:24 -0600

    set icon margins

--- FrontEnd/src/components/NavigationBar.tsx
+++ FrontEnd/src/components/NavigationBar.tsx
@@ -23,15 +23,34 @@ function NavigationBar(props: NavigationBarProps) {
         navigate("/");
     }
 
+    function handleHover(event: React.MouseEvent<HTMLButtonElement>) {
+        const hoverText = document.createElement('div');
+        hoverText.classList.add('hover-text');
+        hoverText.textContent = (event.target as HTMLButtonElement).getAttribute('title');
+        document.body.appendChild(hoverText);
+
+        function updatePosition(event: MouseEvent) {
+            hoverText.style.top = `${event.clientY}px`;
+            hoverText.style.left = `${event.clientX}px`;
+        }
+
+        function removeHoverText() {
+            document.body.removeChild(hoverText);
+        }
+
+        document.addEventListener('mousemove', updatePosition);
+        document.addEventListener('mouseout', removeHoverText);
+    }
+
     return (
         <div className="navigation-bar">
             <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-            <button className="nav-button" onClick={() => props.selectFunction("ChatRoom List")}><FontAwesomeIcon icon={faUsers} title="Group Chat"/></button>
-            <button className="nav-button" onClick={() => props.selectFunction("Direct Message List")}><FontAwesomeIcon icon={faEnvelope} title="Direct Message" /></button>
+            <button className="nav-button" onClick={() => props.selectFunction("ChatRoom List")} onMouseOver={handleHover}><FontAwesomeIcon icon={faUsers} title="Group Chat" /></button>
+            <button className="nav-button" onClick={() => props.selectFunction("Direct Message List" )}onMouseOver={handleHover}><FontAwesomeIcon icon={faEnvelope} title="Direct Message"  /></button>
             {/* <button className="nav-button" onClick={() => alert("Not available at the moment")}>BrainStorm Session History</button>
             <button className="nav-button" onClick={() => alert("Not available at the moment")}>Settings</button> */}
-            <button className="nav-button" onClick={() => logOut()}><FontAwesomeIcon icon={faSignOutAlt} title="Log Out" /></button>
+            <button className="nav-button" onClick={() => logOut()} onMouseOver={handleHover}><FontAwesomeIcon icon={faSignOutAlt} title="Log Out" /></button>
         </div>
     );
 }
