f946d90a8756d2710e518f472bce65bc262a7116
singhr27 <144287500+singhr27@users.noreply.github.com>
2023-11-26 01:40:26 -0600

    added icons for the nav bar

--- FrontEnd/src/components/NavigationBar.tsx
+++ FrontEnd/src/components/NavigationBar.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
 import "../styles/NavigationBar.css";
 import SignalRChatRoom from "../services/ChatRoomConnection";
 import SignalRDirect from "../services/DirectMessageConnection";
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faUsers, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 interface NavigationBarProps {
     selectFunction: (tab: string) => void;
@@ -22,11 +25,13 @@ function NavigationBar(props: NavigationBarProps) {
 
     return (
         <div className="navigation-bar">
-            <button className="nav-button" onClick={() => props.selectFunction("ChatRoom List")}>Chat Rooms</button>
-            <button className="nav-button" onClick={() => props.selectFunction("Direct Message List")}>Direct Message</button>
+            <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+            <button className="nav-button" onClick={() => props.selectFunction("ChatRoom List")}><FontAwesomeIcon icon={faUsers} /></button>
+            <button className="nav-button" onClick={() => props.selectFunction("Direct Message List")}><FontAwesomeIcon icon={faEnvelope} /></button>
             {/* <button className="nav-button" onClick={() => alert("Not available at the moment")}>BrainStorm Session History</button>
             <button className="nav-button" onClick={() => alert("Not available at the moment")}>Settings</button> */}
-            <button className="nav-button" onClick={() => logOut()}>Logout</button>
+            <button className="nav-button" onClick={() => logOut()}><FontAwesomeIcon icon={faSignOutAlt} /></button>
         </div>
     );
 }
