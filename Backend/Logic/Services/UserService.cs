e6985daabab4c6ea1515f8f2617d1d3fe21a2bce
singhr27 <144287500+singhr27@users.noreply.github.com>
2023-11-28 21:29:36 -0600

    more comments

--- Backend/Logic/Services/UserService.cs
+++ Backend/Logic/Services/UserService.cs
@@ -1,19 +1,41 @@
-﻿using Database.CollectionContracts;
+﻿/*
+ * UserService.cs
+ * -------------------------
+ * Represents a UserService object from the database.
+ * This file contains the data for the UserService.
+ * ---------------------------------------------------------
+ * Author: Mr. Roland Fehr
+ * Last modified: 28.10.2023
+ * Version: 1.0
+*/
+
+using Database.CollectionContracts;
 using Logic.DTOs.User;
 using Logic.Exceptions;
 using Logic.Helpers;
 
 namespace Logic.Services
 {
+    /// <summary>
+    ///   This class contains the data for the UserService.
+    /// </summary>
     public class UserService
     {
         private readonly IUserCollection userCollection;
 
+        /// <summary>
+        ///  Constructor for the UserService
+        /// </summary>
+        /// <param name="userCollection"></param>
         public UserService(IUserCollection userCollection)
         {
             this.userCollection = userCollection;
         }
 
+        /// <summary>
+        ///   This method registers a user
+        /// </summary>
+        /// <param name="registerUser">The user to register</param>
         public async Task<FriendlyUserInfo> CreateUser(RegisterUserRequest registerUser)
         {
             // make sure username does not exist
