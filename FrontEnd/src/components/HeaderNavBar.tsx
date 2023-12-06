a8109d92b48bc3ad195b9793c3f5b72c486cfa2e
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2023-12-05 18:39:56 -0600

    Add join code input field and button to HeaderNavBar component

--- FrontEnd/src/components/HeaderNavBar.tsx
+++ FrontEnd/src/components/HeaderNavBar.tsx
@@ -1,4 +1,8 @@
+import {
+  MDBInput
+} from 'mdb-react-ui-kit';
 import { useEffect, useState } from "react";
+import { useDataContext } from "../contexts/DataContext";
 import {
   DisplayTypes,
   ErrorMessages,
@@ -8,28 +12,26 @@ import SignalRChatRoom from "../services/ChatRoomConnection";
 import UserInfo from "../services/UserInfo";
 import "../styles/HeaderNavBar.css";
 import UserProfile from "./UserProfile";
-import { useDataContext } from "../contexts/DataContext";
 
-/*
+interface HeaderNavBarProps {
+  noticeFunction: (msg: NoticeMessages) => void;
+  clickedUserProfile: () => void;
+}
+
+/**
  * HeaderNavBar.tsx
  * -------------------------
  * This component is the header navigation bar of the main page.
  * It contains the user name, join code input field, and the user profile.
  * -----------------------------------------------------------------------
  * Authors:  Mr. Yee Tsung (Jackson) Kao & Mr. Roland Fehr
- * Date:     2021-04-01
- * Version:  1.0
  */
-interface HeaderNavBarProps {
-  noticeFunction: (msg: NoticeMessages) => void;
-  clickedUserProfile: () => void;
-}
-
 function HeaderNavBar(props: HeaderNavBarProps) {
   const context = useDataContext();
   const updateName = context[12];
+  const [joinChatCode, setJoinChatCode] = useState("");
   const [userInfo, setUserInfo] = useState(UserInfo.getUserInfo());
-  const [errorMsg, setErrorMsg] = useState(ErrorMessages.Empty);
+  const [errorMsg, setErrorMsg] = useState<ErrorMessages | NoticeMessages>(ErrorMessages.Empty);
   const [display, setDisplay] = useState(DisplayTypes.None);
 
   /**
@@ -37,21 +39,42 @@ function HeaderNavBar(props: HeaderNavBarProps) {
    * @returns
    */
   async function joinCode() {
+    setDisplay(DisplayTypes.None);
     if (sessionStorage.getItem("isGuest") !== null) {
       props.noticeFunction(NoticeMessages.FeatureRestricted);
       return;
     }
 
-    const code = document.getElementById("JoinCode") as HTMLInputElement;
-    if (code.value === "") {
+    const button = document.getElementsByClassName("JoinCodeButton")[0] as HTMLButtonElement;
+    button.disabled = true; // Disable the button to prevent spamming
+    if (joinChatCode === "") { // Check if the input is empty
       setErrorMsg(ErrorMessages.FormIncomplete);
       setDisplay(DisplayTypes.Flex);
+      button.disabled = false;
       return;
+    } else if (UserInfo.getChatRoomByCode(joinChatCode)) { // Check if the user is already in the chat room
+      setErrorMsg(NoticeMessages.AlreadyInChatRoom);
+      setDisplay(DisplayTypes.Flex);
+      button.disabled = false;
+      return;
+    } else {
+      await SignalRChatRoom.getInstance().then((value) =>
+        value.joinChatRoom(joinChatCode, "First")
+      );
+      button.disabled = false;
     }
-    await SignalRChatRoom.getInstance().then((value) =>
-      value.joinChatRoom(code.value, "First")
-    );
-    code.value = ""; // Clear the input field
+    setJoinChatCode("");
+    (document.getElementById("JoinCodeForm") as HTMLFormElement).reset(); // Reset the form
+  }
+
+  /**
+   * Handle the enter key press
+   * @param value
+   */
+  function handleKey(value: React.KeyboardEvent<HTMLInputElement>) {
+    if (value.key === "Enter" || value.key === "NumpadEnter") {
+      joinCode();
+    }
   }
 
   useEffect(() => {
@@ -62,8 +85,10 @@ function HeaderNavBar(props: HeaderNavBarProps) {
     <div className="HeaderNavBar" >
       <div className="HeaderTitle">BRAINSTORM</div>
       <div className="JoinCodeSection">
-        <input className="JoinCodeInput" type="text" id="JoinCode" placeholder="Chat Room Join Code..." />
-        <button className="JoinCodeButton" onClick={() => joinCode()}>Join</button>
+        <form id='JoinCodeForm' onSubmit={(e) => e.preventDefault()}>
+          <MDBInput wrapperClass='mb-4' label='Chat Room Join Code...' id='JoinCode' type='text' autoComplete='off' value={joinChatCode} onChange={(e) => setJoinChatCode(e.target.value)} onKeyDown={handleKey} />
+        </form>
+        <button className="JoinCodeButton" onClick={joinCode}>Join</button>
         <h5 className="ErrorMsg" style={{ display: display }}>{errorMsg}</h5>
       </div>
       <div className="UserProfile" onClick={props.clickedUserProfile}>
