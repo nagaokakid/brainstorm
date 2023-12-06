6e100015a31d13d587b7e6384e47017091cd95bf
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2023-12-06 13:13:41 -0600

    Fixed the scroll to bottom

--- FrontEnd/src/components/MessageWindow.tsx
+++ FrontEnd/src/components/MessageWindow.tsx
@@ -8,58 +8,63 @@ import "../styles/MessageWindow.css";
 import MessageBox from "./MessageBox";
 import MessageInput from "./MessageInput";
 
-/* 
-    *  MessageWindow.tsx 
-    * -------------------------
-    *  This component is the message window of the chat room.
-    *  It contains the messages of the chat room.
-    *  -----------------------------------------------------------------------
-    * Authors:  Mr. Yee Tsung (Jackson) Kao & Mr. Roland Fehr
-    * Date Created:  01/12/2023
-    * Last Modified: 01/12/2023
-    * Version: 1.0
-    */
+
 interface MessageWindowProps {
     chatId: string;
     chatType: TabTypes;
 }
 
+/**
+*  MessageWindow.tsx 
+* -------------------------
+*  This component is the message window of the chat room.
+*  It contains the messages of the chat room.
+*  -----------------------------------------------------------------------
+* Authors:  Mr. Yee Tsung (Jackson) Kao & Mr. Roland Fehr
+*/
 function MessageWindow(props: MessageWindowProps) {
     const context = useDataContext();
     const msg = context[1];
     const render = context[4];
+    const [trigger, setTrigger] = useState(false); // Trigger to re-render the component
     const [messages, setMessages] = useState([] as (chatRoomMessageObject | { fromUserId: string, messageId: string, message: string, timestamp: string })[]); // Set the message to the display
-
-    function scrollToBottom() {
-        const chatWindow = document.getElementById("MsgSection");
-        if (chatWindow) {
-            chatWindow.scrollTop = chatWindow.scrollHeight;
-        }
-    }
-
-    useEffect(() => {
-        if (msg === undefined) {
-            setMessages(UserInfo.getMessageHistory(props.chatId, props.chatType));
-        } else if ("chatRoomId" in msg && msg.chatRoomId === props.chatId) {
-            setMessages(prev => [...prev, msg]);
-        } else if ("fromUserId" in msg) {
-            setMessages(prev => [...prev, msg]);
-        }
-    }, [msg]);
 
     useEffect(() => {
         setMessages(UserInfo.getMessageHistory(props.chatId, props.chatType));
     }, [props.chatId, render]);
+
+    useEffect(() => {
+        if (trigger) {
+            if (msg === undefined) {
+                setMessages(UserInfo.getMessageHistory(props.chatId, props.chatType));
+            } else if ("chatRoomId" in msg && msg.chatRoomId === props.chatId) {
+                setMessages(prev => [...prev, msg]);
+            } else if ("fromUserId" in msg) {
+                setMessages(prev => [...prev, msg]);
+            }
+        }
+    }, [msg]);
+
+    useEffect(() => {
+        setTrigger(true);
+    }, []);
+
+    useEffect(() => {
+        const chatWindow = document.getElementById("MsgSection");
+        if (chatWindow) {
+            chatWindow.scrollTop = chatWindow.scrollHeight;
+        }
+    }, [messages]);
 
     return (
         <div className="MsgWindowContainer">
             <div className="MsgSection" id="MsgSection">
                 {messages.map((e, index) => (
                     'brainstorm' in e ?
-                        <MessageBox message={e.message} key={index} user={'fromUserInfo' in e ? [e.fromUserInfo.userId, e.fromUserInfo.firstName] : []} isBrainstorm={true} bsId={e.brainstorm?.sessionId} chatId={props.chatId} msgId={'messageId' in e ? e.messageId : ''} chatType={props.chatType} /> :
+                        <MessageBox message={e.message} key={index} user={[e.fromUserInfo.userId, e.fromUserInfo.firstName]} isBrainstorm={true} bsId={e.brainstorm?.sessionId} chatId={props.chatId} msgId={e.messageId} chatType={props.chatType} /> :
                         'fromUserInfo' in e ?
-                            <MessageBox message={e.message} key={index} user={[e.fromUserInfo.userId, e.fromUserInfo.firstName]} isBrainstorm={false} chatId={props.chatId} msgId={'messageId' in e ? e.messageId : ''} chatType={props.chatType} /> :
-                            <MessageBox message={e.message} key={index} user={[e.fromUserId]} isBrainstorm={false} chatId={props.chatId} msgId={'messageId' in e ? e.messageId : ''} chatType={props.chatType} />
+                            <MessageBox message={e.message} key={index} user={[e.fromUserInfo.userId, e.fromUserInfo.firstName]} isBrainstorm={false} chatId={props.chatId} msgId={e.messageId} chatType={props.chatType} /> :
+                            <MessageBox message={e.message} key={index} user={[e.fromUserId]} isBrainstorm={false} chatId={props.chatId} msgId={e.messageId} chatType={props.chatType} />
                 ))}
             </div>
             <div className='InputSection'>
