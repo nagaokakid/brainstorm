c001d9f5e624b62eafe9f269584ae456b3888fc5
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2023-12-14 15:36:35 -0600

    Refactor header navbar styles

--- FrontEnd/src/components/HeaderNavBar.tsx
+++ FrontEnd/src/components/HeaderNavBar.tsx
@@ -1,13 +1,12 @@
 import {
-  MDBInput
+	MDBInput
 } from 'mdb-react-ui-kit';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import { useEffect, useState } from "react";
-import { useDataContext } from "../contexts/DataContext";
+import { useState } from "react";
 import {
-  DisplayTypes,
-  ErrorMessages,
-  NoticeMessages,
+	DisplayTypes,
+	ErrorMessages,
+	NoticeMessages,
 } from "../models/EnumObjects";
 import SignalRChatRoom from "../services/ChatRoomConnection";
 import UserInfo from "../services/UserInfo";
@@ -15,8 +14,8 @@ import "../styles/HeaderNavBar.css";
 import UserProfile from "./UserProfile";
 
 interface HeaderNavBarProps {
-  noticeFunction: (msg: NoticeMessages) => void;
-  clickedUserProfile: () => void;
+	noticeFunction: (msg: NoticeMessages) => void;
+	clickedUserProfile: () => void;
 }
 
 /**
@@ -28,75 +27,67 @@ interface HeaderNavBarProps {
  * Authors:  Mr. Yee Tsung (Jackson) Kao & Mr. Roland Fehr
  */
 function HeaderNavBar(props: HeaderNavBarProps) {
-  const context = useDataContext();
-  const updateName = context[12];
-  const [joinChatCode, setJoinChatCode] = useState("");
-  const [userInfo, setUserInfo] = useState(UserInfo.getUserInfo());
-  const [errorMsg, setErrorMsg] = useState<ErrorMessages | NoticeMessages>(ErrorMessages.Empty);
-  const [display, setDisplay] = useState(DisplayTypes.None);
+	const [joinChatCode, setJoinChatCode] = useState(""); // Set the join chat code
+	const [errorMsg, setErrorMsg] = useState<ErrorMessages | NoticeMessages>(ErrorMessages.Empty); // Set the error message
+	const [display, setDisplay] = useState(DisplayTypes.None); // Set the display of the error message
 
-  /**
-   * Join the chat room with the given code
-   * @returns
-   */
-  async function joinCode() {
-    setDisplay(DisplayTypes.None);
-    if (sessionStorage.getItem("isGuest") !== null) {
-      props.noticeFunction(NoticeMessages.FeatureRestricted);
-      return;
-    }
+	/**
+	 * Join the chat room with the given code
+	 * @returns
+	 */
+	async function joinCode() {
+		setDisplay(DisplayTypes.None);
+		if (sessionStorage.getItem("isGuest") !== null) {
+			props.noticeFunction(NoticeMessages.FeatureRestricted);
+			return;
+		}
 
-    const button = document.getElementsByClassName("JoinCodeButton")[0] as HTMLButtonElement;
-    button.disabled = true; // Disable the button to prevent spamming
-    if (joinChatCode === "") { // Check if the input is empty
-      setErrorMsg(ErrorMessages.FormIncomplete);
-      setDisplay(DisplayTypes.Flex);
-      button.disabled = false;
-      return;
-    } else if (UserInfo.getChatRoomByCode(joinChatCode)) { // Check if the user is already in the chat room
-      setErrorMsg(NoticeMessages.AlreadyInChatRoom);
-      setDisplay(DisplayTypes.Flex);
-      button.disabled = false;
-      return;
-    } else {
-      await SignalRChatRoom.getInstance().then((value) => {
-        value.joinChatRoom(joinChatCode, "First")
-      });
-      button.disabled = false;
-    }
-    setJoinChatCode("");
-    (document.getElementById("JoinCodeForm") as HTMLFormElement).reset(); // Reset the form
-  }
+		const button = document.getElementsByClassName("join-code-button")[0] as HTMLButtonElement;
+		button.disabled = true; // Disable the button to prevent spamming
+		if (joinChatCode === "") { // Check if the input is empty
+			setErrorMsg(ErrorMessages.FormIncomplete);
+			setDisplay(DisplayTypes.Flex);
+			button.disabled = false;
+		} else if (UserInfo.getChatRoomByCode(joinChatCode)) { // Check if the user is already in the chat room
+			setErrorMsg(NoticeMessages.AlreadyInChatRoom);
+			setDisplay(DisplayTypes.Flex);
+			button.disabled = false;
+		} else {
+			await SignalRChatRoom.getInstance().then(async (value) => {
+				await value.joinChatRoom(joinChatCode, "First")
+			});
+			button.disabled = false;
+		}
+		setJoinChatCode("");
+		(document.getElementById("JoinCodeForm") as HTMLFormElement).reset(); // Reset the form
+		return;
+	}
 
-  /**
-   * Handle the enter key press
-   * @param value
-   */
-  function handleKey(value: React.KeyboardEvent<HTMLInputElement>) {
-    if (value.key === "Enter" || value.key === "NumpadEnter") {
-      joinCode();
-    }
-  }
+	/**
+	 * Handle the enter key press
+	 * @param value
+	 */
+	function handleKey(value: React.KeyboardEvent<HTMLInputElement>) {
+		if (value.key === "Enter" || value.key === "NumpadEnter") {
+			joinCode();
+		}
+	}
 
-  useEffect(() => {
-    setUserInfo(UserInfo.getUserInfo());
-  }, [updateName]);
-
-  return (
-    <div className="HeaderNavBar" >
-      <div className="HeaderTitle">BRAINSTORM</div>
-      <div className="JoinCodeSection">
-        <form id='JoinCodeForm' onSubmit={(e) => e.preventDefault()}>
-          <MDBInput wrapperClass='mb-4' label='Chat Room Join Code...' id='JoinCode' type='text' autoComplete='off' value={joinChatCode} onChange={(e) => setJoinChatCode(e.target.value)} onKeyDown={handleKey} />
-        </form>
-        <button className="JoinCodeButton" onClick={joinCode}>Join</button>
-        <h5 className="ErrorMsg" style={{ display: display }}>{errorMsg}</h5>
-      </div>
-      <div className="UserProfile" onClick={props.clickedUserProfile}>
-        <UserProfile user={userInfo} />
-      </div>
-    </div>
-  );
+	return (
+		<div className="header-navbar" >
+			<div className="header-title">BRAINSTORM</div>
+			<div className="code-input-section">
+				<form id='JoinCodeForm' onSubmit={(e) => e.preventDefault()}>
+					<MDBInput wrapperClass='mb-4' label='Chat Room Join Code...' id='JoinCode' type='text' autoComplete='off' value={joinChatCode} onChange={(e) => setJoinChatCode(e.target.value)} onKeyDown={handleKey} />
+				</form>
+				<button className="join-code-button" onClick={joinCode}>Join</button>
+				<h5 className="error-msg" style={{ display: display }}>{errorMsg}</h5>
+			</div>
+			<div className="user-profile" onClick={props.clickedUserProfile}>
+				<UserProfile />
+			</div>
+		</div>
+	);
 }
 
 export default HeaderNavBar;
