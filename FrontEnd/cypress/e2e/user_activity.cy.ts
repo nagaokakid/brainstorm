358559447e9631e6fbe98d7028829ee44174f7d6
nagaokakid <nagaokakid@gmail.com>
2023-12-14 17:12:06 -0600

    Edited e2e tests. All pass.

--- FrontEnd/cypress/e2e/user_activity.cy.ts
+++ FrontEnd/cypress/e2e/user_activity.cy.ts
@@ -17,8 +17,8 @@ describe('Testing Main Page (Logged In User)', () =>
 {
     const username: string = 'cypress';
     const password: string = 'cypress';
-    const foreignJoinCode: string = "461250";
-    const foreignChatRoomTitle = "Movies";
+    const foreignJoinCode: string = "134585";
+    const foreignChatRoomTitle = "foreign room";
     var newChatRoomTitle: string;
     var newChatRoomDescription: string;
 
@@ -33,6 +33,9 @@ describe('Testing Main Page (Logged In User)', () =>
       
         return randomString;
     }
+
+    function goToChatRooms() { cy.get('.NavBar').find('.NavBtn').eq(0).click(); }
+    function goToDirectMessages() { cy.get('.NavBar').find('.NavBtn').eq(1).click(); }
     
     // login as existing user before each test
     beforeEach(() => 
@@ -45,27 +48,11 @@ describe('Testing Main Page (Logged In User)', () =>
         cy.get('#login').click();
     })
 
-    // Test 1
-    it('view all chat rooms', () => 
-    {
-        cy.get('.navigation-bar').find('.nav-button').contains('Chat Rooms').click();
-        cy.get('.CreateChatRoomButton').should('exist');
-        cy.get('.chat-list').find('.chats').children().should('have.length.gt', 0);
-    })
-
-    // Test 2
-    it('view all direct messages', () => 
-    {
-        cy.get('.navigation-bar').find('.nav-button').contains('Direct Message').click();
-        cy.get('.CreateChatRoomButton').should('be.hidden');
-        cy.get('.chat-list').find('.chats').children().should('have.length.gt', 0);
-    })
-
-    // Test 3
+    // Create chat room test
     it('create a new chat room', () => 
     {
-        cy.get('.navigation-bar').find('.nav-button').contains('Chat Rooms').click();
-        cy.get('.CreateChatRoomButton').click();
+        goToChatRooms();
+        cy.get('.CreateChatRoomIcon').click()
 
         newChatRoomTitle = generateRandomString(10);     // unique title and desc
         newChatRoomDescription = generateRandomString(20);
@@ -73,83 +60,172 @@ describe('Testing Main Page (Logged In User)', () =>
         cy.get('.WindowSectionContent').find('#description').type(newChatRoomDescription);
 
         cy.intercept('POST', '/api/chatroom').as('response');
-        cy.get('.WindowSectionContent').find('.submitButton').click();  // create chat room
+        cy.get('.WindowSectionContent').find('.SubmitButton').click();  // create chat room
         cy.wait('@response').then((interception) => 
         {
             expect(interception.response.statusCode).to.equal(200);     // verify response code
         })
 
         cy.wait(3000);
+
+        goToChatRooms();
         
         // verify new chat room exists in chat list
-        cy.get('.chat-list').find('.chats').find('.chat-item').last().then(($lastChild) => {
-            cy.wrap($lastChild).find('.chat-details').find('.chat-title').invoke('text').should('eq', newChatRoomTitle);
-            cy.wrap($lastChild).find('.chat-details').find('.last-message').invoke('text').should('eq', newChatRoomDescription);
+        cy.get('.ListContainer').find('.ListItem').last().within(() => {
+            cy.get('.ItemTitle').should('have.text', newChatRoomTitle);
+            cy.get('.LastMessage').should('have.text', newChatRoomDescription);
         });
     })
 
-    // Test 4
+    // Send chat room message test
     it('send a message in a chat room', () => 
     {
-        cy.get('.navigation-bar').find('.nav-button').contains('Chat Rooms').click();
-        cy.get('.chats').find('.chat-title').contains(newChatRoomTitle).click();
+        goToChatRooms();
+        cy.get('.ListContainer').find('.ListItem').last().click();
         let msg = generateRandomString(10);
         cy.get('.MsgInputContainer').find('[placeholder="Enter Message here..."]').type(msg);
-        cy.get('.send').click(); // send msg
+        cy.get('.Send').click(); // send msg
+        goToChatRooms();
+        cy.get('.ListContainer').find('.ListItem').last().click();
+        cy.wait(1500);
 
         // verify message is present in chat window
-        cy.get('.MsgWindowContainer').find('.MsgSection').find('.MessageContainer').last().then(($lastChild) => 
+        cy.get('.MessageContainer').find('.MessageWrapper').last().within(() => 
         {
-            cy.wrap($lastChild).find('p').contains(msg).should('exist');
+            cy.get('.Message').find('p').should('have.text', msg);
         })
     })
 
-    // Test 5
+    // Join chat room test
     it('join a chat room via 6-digit code', () =>
     {
-        cy.get('.JoinCodeInput').type(foreignJoinCode);
-        cy.get('.JoinCodeButton').click();
+        cy.wait(3000);
+        cy.get('#JoinCodeForm').type(foreignJoinCode);
+        cy.get('.JoinCodeButton').click();  // join foreign chat room
+        cy.wait(2000);
+        goToChatRooms();
+        cy.get('.ListContainer').find('.ListItem').contains('.ItemTitle', foreignChatRoomTitle);
     })
 
-    // Test 6
+    // Edit a chat room test
+    it("edit a chat room's title and description", () => 
+    {
+        // pull up the edit chat room window
+        cy.get('.ListContainer').find('.ListItem').contains('.ItemTitle', newChatRoomTitle).parent().parent().find('div[style="display: flex;"]').find('.EditChatRoomButton').invoke('show').click();
+
+        // set up intercept for expected API response
+        cy.intercept('PUT', '/api/chatroom').as('response');
+
+        // edit title and desc
+        cy.get('.EditChatRoomForm').find('#ChatRoomTitle').clear().type('editedTitle');
+        cy.get('.EditChatRoomForm').find('#ChatRoomDescription').clear().type('editedDescription');
+        cy.get('.SaveEditChatRoomButton').click();
+
+        // verify correct API response
+        cy.wait('@response').then((interception) => 
+        {
+            expect(interception.response.statusCode).to.equal(200);
+        })
+
+        goToChatRooms();
+        cy.get('.ListContainer').find('.ListItem').contains('.ItemTitle', 'editedTitle').should('exist');
+    })
+
+    // Send direct message test
     it('send a direct message to chat room member via chat room', () => 
     {
-        // send direct message (user must manually fill in prompt)
-        cy.get('.navigation-bar').find('.nav-button').contains('Chat Rooms').click();
-        cy.get('.chats').find('.chat-title').contains('Movies').click();
-        cy.get('.MemberListContainer').find('li').contains('string').click();
-        cy.log('back from prompt...now checking direct message');
+        var msg = generateRandomString(10);
+
+        // send direct message
+        goToChatRooms();
+        cy.get('.ListContainer').find('.ListItem').contains('.ItemTitle', foreignChatRoomTitle).click();
+        cy.get('.ChatMemberList')
+            .find('.MembersList')
+            .find('ul.Members')
+            .find('.UserProfileName')
+            .contains('cypress2')
+            .parent().parent()
+            .then(member => {
+                cy.wrap(member).click();
+                cy.wrap(member).find('.DMInputForm').type(msg);
+                cy.wait(1000);
+                cy.wrap(member).find('button').click();
+            });
 
         // check direct message list
-        cy.get('.navigation-bar').find('.nav-button').contains('Direct Message').click();
-        let target = cy.get('.chats').find('.chat-item').find('.chat-title:contains("string")').should('exist');
-        target.click();
-        cy.get('.MsgSection').find('.MsgWindowContainer').find('.MsgSection').children().should('have.length.gt', 0);
+        goToDirectMessages();
+        
+        cy.get('.ListContainer').find('.ListItem').contains('.ItemTitle', 'cypress2').click();
+        var lastMsg = cy.get('#MsgSection').children().last();
+        lastMsg.find('.Message').find('p').should('have.text', msg);
     })
 
-    // Test 7
+    // Leave a chat room test
+    it('leave a chat room', () => 
+    {
+        var prevCount = 0;
+        cy.intercept('DELETE', '/api/chatroom').as('response');
+
+        cy.get('.ListContainer').children().then(children => {
+            prevCount = children.length;
+        
+            // leave the chat room
+            cy.get('.ListContainer')
+                .find('.ListItem')
+                .contains('.ItemTitle', foreignChatRoomTitle)
+                .parent()
+                .parent()
+                .find('div[style="display: flex;"]')
+                .find('.EditChatRoomButton')
+                .invoke('show')
+                .click();
+            cy.get('.EditChatRoomButtonsContainer').find('.LeaveChatRoomButton').click();
+            
+            // wait to ensure previous commands are complete
+            cy.wait(3000);
+
+            // verify API response
+            cy.wait('@response').then((interception) => 
+            {
+                expect(interception.response.statusCode).to.equal(200);
+            })
+        
+            goToChatRooms();
+        
+            // verify new count
+            cy.get('.ListContainer').children().should('have.length', prevCount - 1);
+        })
+    })
+
+    // Send direct message in pre-existing direct message window test
     it('send a direct message to another user via pre-existing direct message window', () => 
     {
-        cy.get('.navigation-bar').find('.nav-button').contains('Direct Message').click();
-        cy.get('.chats').find('.chat-item').find('.chat-title').first().click();
+        goToDirectMessages();
+        cy.get('.ListContainer').children().first().click();
         let msg = generateRandomString(10);
         cy.get('.MsgInputContainer').find('[placeholder="Enter Message here..."]').type(msg);
-        cy.get('.send').click(); // send msg
+        cy.get('.Send').click(); // send msg
+        cy.wait(1000);
         cy.get('.MsgWindowContainer').find('.MsgSection').find('.MessageContainer').contains(msg).should('exist');
     })
 
-    // Test 8
+    // Brainstorm session features test
     it('create a brainstorm session, input ideas, vote, and leave the session', () =>
     {
         // create brainstorm session
-        cy.get('.navigation-bar').find('.nav-button').contains('Chat Rooms').click();
-        cy.get('.chats').find('.chat-item').find('.chat-title').first().click();
-        cy.get('.ChatHeader').find('a').contains('Create BrainStorm Session').click();
-        var bsInfo = cy.get('.BSinfoWindow');
-        bsInfo.get('#BSname').type('test session');
-        bsInfo.get('#BSdescription').type('for cypress e2e');
-        bsInfo.get('#BStimer').type('10');
-        bsInfo.get('#createBs').click();
+        goToChatRooms();
+        cy.get('.ListContainer').children().first().click();
+        cy.get('.ChatHeader').find('a').contains('Create Brainstorm Session').click();
+
+        // fill out session info
+        cy.get('.CreateBSForm').then((BSform) => {
+            cy.wrap(BSform).find('#BSname').type('test session');
+            cy.wrap(BSform).find('#BSdescription').type('for cypress e2e');
+            cy.wrap(BSform).find('#BStimer').type('15');
+        })
+
+        // create session
+        cy.get('.BSinfoWindow').find('.CreateBtn').click();
         
         // start session and add 4 ideas
         cy.url().should('contain', '/BrainStorm');
@@ -196,12 +272,28 @@ describe('Testing Main Page (Logged In User)', () =>
         })
 
         // End voting and verify the results
-        cy.get('.EndVoteButton').click();
+        cy.get('.EndVoteButton').first().click();
         cy.get('.OnlineIdeasContainer').children().should('have.length', 1);
         cy.get('.OnlineIdeasContainer').find('.IdeaResultBox').find('.IdeaThought').should('contain', 'idea 2');
-        cy.get('.ExitButton').click();
-        cy.get('.YesBtn').click();
+        cy.get('.LeaveSessionButton').click();
         cy.url().should('contain', '/main');
+    })
+
+    // View chat rooms test
+    it('view all chat rooms', () => 
+    {
+        goToChatRooms();
+        cy.get('.CreateChatRoomIcon').should('exist');
+        cy.get('.ChatRoomHeader').should('contain', "Chat Rooms");
+        cy.get('.ListContainer').children().should('have.length.gt', 0);
+    })
+
+    // View direct messages test
+    it('view all direct messages', () => 
+    {
+        goToDirectMessages();
+        cy.get('.DirectMessageHeader').should('contain', "Direct Messages");
+        cy.get('.ListContainer').children().should('have.length.gt', 0);
     })
 
 
