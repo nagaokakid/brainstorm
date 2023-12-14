f1b76fe0491258a0d253f5ac61f54e2f66e01fee
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2023-12-14 16:06:40 -0600

    Refactor NavigationBar component and its styles

--- FrontEnd/src/components/NavigationBar.tsx
+++ FrontEnd/src/components/NavigationBar.tsx
@@ -6,30 +6,26 @@ import SignalRChatRoom from "../services/ChatRoomConnection";
 import SignalRDirect from "../services/DirectMessageConnection";
 import "../styles/NavigationBar.css";
 
-/* 
- * NavigationBar.tsx
-    * -------------------------
-    * This component is the navigation bar of the chat page.
-    * It contains the navigation buttons for the user to navigate between the chat room list and direct message list.
-    * ----------------------------------------------------------------------------------------------------------------
-    * Authors:  Ravdeep Singh
-    * Date Created:  01/12/2023
-    * Last Modified: 01/12/2023
-    * Version: 1.0
-    */
-
 interface NavigationBarProps {
     selectFunction: (tab: TabTypes) => void;
     activeTab: TabTypes;
 }
 
+/**
+* NavigationBar.tsx
+* -------------------------
+* This component is the navigation bar of the chat page.
+* It contains the navigation buttons for the user to navigate between the chat room list and direct message list.
+* ----------------------------------------------------------------------------------------------------------------
+* Authors:  Ravdeep Singh
+*/
 function NavigationBar(props: NavigationBarProps) {
     const navigate = useNavigate();
 
     /**
      * Logs the user out of the application and remove the token from the local storage
      */
-    async function logOut() {
+    async function handleLogoutButton() {
         await SignalRChatRoom.getInstance().then((value) => value.reset());
         await SignalRDirect.getInstance().then((value) => value.reset());
         sessionStorage.clear();
@@ -37,24 +33,16 @@ function NavigationBar(props: NavigationBarProps) {
     }
 
     return (
-        <div className="NavBar">
+        <div className="navigation-bar-container">
             <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-            <button
-                className={`NavBtn ${props.activeTab === TabTypes.ChatRoom ? 'active' : ''}`}
-                onClick={() => props.selectFunction(TabTypes.ChatRoom)}
-                
-            >
+            <button className={`navBtn ${props.activeTab === TabTypes.ChatRoom ? 'active' : ''}`} onClick={() => props.selectFunction(TabTypes.ChatRoom)}>
                 <FontAwesomeIcon icon={faUsers} title="Group Chat" />
             </button>
-            <button
-                className={`NavBtn ${props.activeTab === TabTypes.DiretMessage ? 'active' : ''}`}
-                onClick={() => props.selectFunction(TabTypes.DiretMessage)}
-                
-            >
+            <button className={`navBtn ${props.activeTab === TabTypes.DiretMessage ? 'active' : ''}`} onClick={() => props.selectFunction(TabTypes.DiretMessage)}>
                 <FontAwesomeIcon icon={faEnvelope} title="Direct Message" />
             </button>
-            <button className="NavBtn" onClick={() => logOut()}><FontAwesomeIcon icon={faSignOutAlt} title="Log Out" /></button>
+            <button className="navBtn" onClick={() => handleLogoutButton()}><FontAwesomeIcon icon={faSignOutAlt} title="Log Out" /></button>
         </div>
     );
 }
