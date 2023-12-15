a2a0b72dffd6ed766df5b0c273b409871d9b9e34
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2023-12-14 21:00:45 -0600

    Refactor local idea list component and delete member list component

--- FrontEnd/src/components/BS_LocalIdeaList.tsx
+++ FrontEnd/src/components/BS_LocalIdeaList.tsx
@@ -3,26 +3,26 @@ import UserInfo from '../services/UserInfo';
 import { useEffect, useState } from 'react';
 import deleteIcon from '../assets/delete.png'
 
-/*
- *  BS_LocalIdeaList.tsx 
- * -------------------------
- *  This component is the local idea list of the brain storm page.
- *  It contains the list of ideas that the user has created.
- *  -----------------------------------------------------------------------
- * Author:  Mr. Yee Tsung (Jackson) Kao
- * Date Created:  01/12/2023
- * Last Modified: 01/12/2023
- * Version: 1.0
- */
 interface BS_LocalIdeaListProps {
     content: string[],
 }
 
+/**
+ *  BS_LocalIdeaList.tsx 
+ * -------------------------
+ *  This component is the local idea list of the brain storm page.
+ *  It contains the list of ideas that the user has created.
+ * -----------------------------------------------------------------------
+ *  Author:  Mr. Yee Tsung (Jackson) Kao
+ */
 function BS_LocalIdeaList(props: BS_LocalIdeaListProps) {
-
     const [display, setDisplay] = useState("none");
     const [forceUpdate, setForceUpdate] = useState(false);
 
+    /**
+     * Handle the delete click event
+     * @param position The position of the idea in the list
+     */
     function handleDeleteClick(position: number) {
         UserInfo.deleteIdea(position);
         props.content.splice(position, 1);
@@ -35,18 +35,17 @@ function BS_LocalIdeaList(props: BS_LocalIdeaListProps) {
         } else {
             setDisplay("block");
         }
-
     }, [props.content, forceUpdate]);
 
     return (
-        <div className="LocalIdeasContainer" style={{ display: display }}>
+        <div className="local-ideas-container" style={{ display: display }}>
             <h1>Current Ideas:</h1>
             {props.content.map((idea, id) => {
                 return (
                     <div className="Idea" key={id}>
                         {idea}
-                        <button className='DeleteButton' onClick={() => handleDeleteClick(id)}>
-                            <img src={deleteIcon} width={20} height={20}/>
+                        <button className='local-idea-delete-button' onClick={() => handleDeleteClick(id)}>
+                            <img src={deleteIcon} width={20} height={20} />
                         </button>
                     </div>
                 );
