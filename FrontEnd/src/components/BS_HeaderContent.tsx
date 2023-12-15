e1830c2d06be66721fae83d8d940e59131211e91
Jackson <117477669+Jacksonkao97@users.noreply.github.com>
2023-12-14 21:05:33 -0600

    Refactor BS_HeaderContent component and its CSS

--- FrontEnd/src/components/BS_HeaderContent.tsx
+++ FrontEnd/src/components/BS_HeaderContent.tsx
@@ -4,54 +4,51 @@ import userIcon from "../assets/group-chat.png";
 import hourGlassIcon from "../assets/whiteHourGlass.png";
 
 interface BS_HeaderContentProps {
-  roomTitle: string;
-  roomDescription: string;
-  timer: number;
-  memberCount: number;
-  creatorId: string;
+    roomTitle: string;
+    roomDescription: string;
+    timer: number;
+    memberCount: number;
+    creatorId: string;
 }
 
 function BS_HeaderContent(props: BS_HeaderContentProps) {
-  useEffect(() => {
-    const iconButton = document.getElementById("InfoIcon");
-    if (iconButton) {
-      iconButton.addEventListener("mouseover", () => {
-        const infoContent = document.getElementsByClassName(
-          "InfoContent"
-        )[0] as HTMLElement;
-        infoContent.style.display = "flex";
-      });
-      iconButton.addEventListener("mouseout", () => {
-        const infoContent = document.getElementsByClassName(
-          "InfoContent"
-        )[0] as HTMLElement;
-        infoContent.style.display = "none";
-      });
-    }
-  }, []);
+    useEffect(() => {
+        const iconButton = document.getElementById("InfoIcon");
+        if (iconButton) {
+            iconButton.addEventListener("mouseover", () => {
+                const infoContent = document.getElementsByClassName(
+                    "InfoContent"
+                )[0] as HTMLElement;
+                infoContent.style.display = "flex";
+            });
+            iconButton.addEventListener("mouseout", () => {
+                const infoContent = document.getElementsByClassName(
+                    "InfoContent"
+                )[0] as HTMLElement;
+                infoContent.style.display = "none";
+            });
+        }
+    }, []);
 
-  return (
-    <div className="HeaderContent">
-      <div className="BS_MemberCount">
-        <img
-          className="JoinedIcon_BS"
-          src={userIcon}
-          style={{ fontWeight: "bold" }}
-        />
-        <div>{props.memberCount}</div>
-      </div>
-      <div className="HeaderContentTitle">
-        <div className="BS_Title">{props.roomTitle}</div>
-        <div className="BS_Description">{props.roomDescription}</div>
-      </div>
-      <div className="BS_Timer">
-        <img src={hourGlassIcon} width={30} /> {props.timer.toString()} sec
-      </div>
-      {/* <div className="Note" style={{ display: UserInfo.isHost(props.creatorId) ? "flex" : "none" }}>
-        <p>You Are The Host</p>
-      </div> */}
-    </div>
-  );
+    return (
+        <div className="header-content">
+            <div className="bs_member-count">
+                <img
+                    className="joined-icon-bs"
+                    src={userIcon}
+                    style={{ fontWeight: "bold" }}
+                />
+                <div>{props.memberCount}</div>
+            </div>
+            <div className="header-content-title">
+                <div className="bs-title">{props.roomTitle}</div>
+                <div className="bs-description">{props.roomDescription}</div>
+            </div>
+            <div className="bs-timer">
+                <img src={hourGlassIcon} width={30} /> {props.timer.toString()} sec
+            </div>
+        </div>
+    );
 }
 
 export default BS_HeaderContent;
